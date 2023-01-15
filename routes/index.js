702fe25e317c1ced8db0880f0fff3688d9a8c779
Alexei Scheglov <aleshascheglov@yandex.ru>
2023-01-15 12:30:06 +0300

    fix: Обработка неправильного пути

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const { users } = require('./users');
 const { cards } = require('./cards');
+const { handleError } = require('../utils/error');
 
 const routes = express.Router();
 
 routes.use('/users', users);
 routes.use('/cards', cards);
+routes.all('*', (req, res) => {
+  const err = new Error('Неверный адрес запроса');
+  err.name = 'NotFoundError';
+  handleError(err, req, res);
+});
 
 module.exports = { routes };
