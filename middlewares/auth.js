3c7d8a657980a268306dc0d412c09c883de95606
Alexei Scheglov <aleshascheglov@yandex.ru>
2023-01-17 00:10:29 +0300

    fix pr14

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -4,24 +4,28 @@ const { UnauthorizedError } = require('../errors/UnauthorizedError');
 const extractBearerToken = (header) => header.replace('Bearer ', '');
 
 function auth(req, res, next) {
-  const { authorization } = req.headers;
-
-  if (!authorization || !authorization.startsWith('Bearer ')) {
-    throw new UnauthorizedError('Ошибка авторизации');
-  }
-
-  const token = extractBearerToken(authorization);
-  let payload;
-
   try {
-    payload = jwt.verify(token, 'secretkey');
+    const { authorization } = req.headers;
+
+    if (!authorization || !authorization.startsWith('Bearer ')) {
+      throw new UnauthorizedError('Ошибка авторизации');
+    }
+
+    const token = extractBearerToken(authorization);
+    let payload;
+
+    try {
+      payload = jwt.verify(token, 'secretkey');
+    } catch (err) {
+      throw UnauthorizedError('Ошибка авторизации');
+    }
+
+    req.user = payload; // записываем пейлоуд в объект запроса
+
+    next(); // пропускаем запрос дальше
   } catch (err) {
-    throw UnauthorizedError('Ошибка авторизации');
+    next(err);
   }
-
-  req.user = payload; // записываем пейлоуд в объект запроса
-
-  next(); // пропускаем запрос дальше
 }
 
 module.exports = { auth };
--- models/user.js
+++ models/user.js
@@ -31,6 +31,7 @@ const userSchema = new mongoose.Schema({
   avatar: {
     type: String,
     default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
+    alt: 'Картинка',
     validate: {
       validator: (v) => validator.isURL(v),
       message: 'Некорректный URL',
